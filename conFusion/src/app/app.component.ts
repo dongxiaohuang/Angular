c05983a71f3139eb7729e752b2fe1c354a6580ed
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-05-28 15:03:04 +0100

    - set up components - generate menu component

--- conFusion/src/app/app.component.ts
+++ conFusion/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//conponent operator takes an javascript object as a parameter
 @Component({
+// used in index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'confusion';
 }
+// a component is just a js/ts class
--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+// import { MaterialModule } from '@angular/material';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+//decorator function
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
